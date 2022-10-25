1e816feafbbb4fde7d7cb65749e0939996849b75
Taylor Allen <taylorallen0913@gmail.com>
2022-10-25 10:57:37 -0700

    done

--- main.py
+++ main.py
@@ -6,4 +6,24 @@ num2 = list(map(int, input().split()))
 # Make your Code
 # ******************************
 
-# print (num3) 
+num3 = []
+counter = 0
+
+final_len = len(num1) + len(num2)
+
+while len(num3) <= final_len:
+    if len(num1) == 0:
+        num3 = num3 + num2
+        break
+    elif len(num2) == 0:
+        num3 = num3 + num1
+        break
+
+    if counter % 2 == 0:
+        num3.append(num1.pop(0))
+    elif counter % 2 == 1:
+        num3.append(num2.pop(0))
+
+    counter += 1
+
+print(num3)
